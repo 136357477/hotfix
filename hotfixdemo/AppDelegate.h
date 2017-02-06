dda27b12e1a10d7360f8e016902257da3ef16df2
guohui <guohui136357477@126.com>
2017-02-06 17:15:49 +0800

    Initial Commit

---
+++ hotfixdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hotfixdemo
+//
+//  Created by guohui on 2017/2/6.
+//  Copyright © 2017年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hotfixdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hotfixdemo
+//
+//  Created by guohui on 2017/2/6.
+//  Copyright © 2017年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
